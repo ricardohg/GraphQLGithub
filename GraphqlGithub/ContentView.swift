1ad0af4763a046a550b43aa881b95b971c7f3b2a
Ricardo Hernandez <a.ricardohg@gmail.com>
2021-01-27 09:19:00 -0500

    initial commit

---
+++ GraphqlGithub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraphqlGithub
+//
+//  Created by ricardo hernandez  on 27/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraphqlGithub/GraphqlGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphqlGithubApp.swift
+//  GraphqlGithub
+//
+//  Created by ricardo hernandez  on 27/01/21.
+//
+
+import SwiftUI
+
+@main
+struct GraphqlGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
