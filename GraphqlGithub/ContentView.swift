21b8b50a622dac5a8f2cec898122d417ae7ad4e7
Ricardo Hernandez <a.ricardohg@gmail.com>
2021-01-28 19:28:27 -0500

    added tabbar

--- GraphqlGithub/ContentView.swift
+++ GraphqlGithub/ContentView.swift
@@ -10,10 +10,26 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        
-        NavigationView {
-            RepositoryListView()
+        TabView {
+            NavigationView {
+                RepositoryListView()
+            }.tabItem {
+                Image(systemName: "externaldrive.badge.icloud")
+                Text("GraphQL Repositories")
+            }
+            
+            NavigationView {
+                RepositoryListView()
+                   
+            }.tabItem {
+                Image(systemName: "heart")
+                Text("Favorites")
+            }
+            
+            
         }
+        
+    
          
     }
 }
--- GraphqlGithub/Views/RepositoryListView.swift
+++ GraphqlGithub/Views/RepositoryListView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SafariServices
 
 struct RepositoryListView: View {
     
